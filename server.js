04ffafa198519853113b8a6bc11dfac349d7adfc
Abdullah-git-hub <74533464+Abdullah-git-hub@users.noreply.github.com>
2024-08-15 17:12:06 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express")
 const server = express()
 
+const port = process.env.PORT || 3000;
+
 server.all("/", (req, res) => {
   res.send("Bot is running!")
 })
 
 function stayLive() {
-  server.listen(3000, () => {
+  server.listen(port, () => {
     console.log("Server is ready.")
   })
 }
